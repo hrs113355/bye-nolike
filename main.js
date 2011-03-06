a3d6fc638dfb671407605f2d42310037f054abb4
hrs <hrs113355@gmail.com>
2011-03-07 02:28:23 +0800

    fix loop

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 if(typeof(pid)== "number"){ajaxget('forum.php?mod=viewthread&show=true&tid=' + tid + '&viewpid=' + pid, 'post_' + pid);}
-location.replace(document.URL+'&show=true');
+if (!(document.URL.match(/&show=true/))){
+    location.replace(document.URL+'&show=true');
+}
