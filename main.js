68a5da6ecb19538a65208c7f7d57a5ac9c0d4382
hrs <hrs113355@gmail.com>
2011-03-15 21:41:34 +0800

    add license

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 if(typeof(pid)== "number"){ajaxget('forum.php?mod=viewthread&show=true&tid=' + tid + '&viewpid=' + pid, 'post_' + pid);}
 else if (document.URL.match(/ideapit.net/)){
     change();
