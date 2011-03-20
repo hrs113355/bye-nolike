56898ed09042ebf54e24a66024f46fcb415731d8
Zhusee Zhang <zhusee2@gmail.com>
2011-03-20 16:26:58 +0800

    inject warning dialog into like-abuse pages

---
+++ bye-nolike.safariextension/javascripts/inject.js
@@ -0,0 +1,49 @@
+if(window.top === window) {
+
+var ext = safari.extension;
+var baseURI = ext.baseURI;
+
+function needLikeToRead() {
+  switch(location.hostname) {
+    case 'tenkucity.com':
+      if($('div.like_locked').length > 0) {
+        return true;
+        break;
+      }
+    case 'citytalk.tw':
+      if($('table div.locked').length > 0) {
+        return true;
+        break;
+      }
+    case 'ideapit.net':
+      if($('table div#hiden_box').length > 0) {
+        return true;
+        break;
+      }
+    default:
+      return false;
+      break;
+  }
+}
+
+function isCrackable() {
+  return false;
+}
+
+function displayAlert(crackable) {
+  crackable = crackable || false;
+  
+  $('<iframe>').css({
+      width: '100%', height: '100%', border: 'none',
+      position: 'fixed', top: 0, left: 0, zIndex: 9999
+    })
+    .attr('id', 'by_nolike_warning')
+    .attr('src', baseURI + 'warning.html?title=' + document.title + (crackable ? '' : '&crackable=false') )
+    .appendTo($('body'));
+}
+
+if(needLikeToRead()) {
+  displayAlert(isCrackable());
+}
+
+}
