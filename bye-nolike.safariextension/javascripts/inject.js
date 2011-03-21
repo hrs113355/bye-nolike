8ab0cf0609c91e129a1033cb0485f202d08e0885
Zhusee Zhang <zhusee2@gmail.com>
2011-03-21 13:19:55 +0800

    Inject warning windows directly into DOM rather than the previous iframe approach.
    Due to security reasons, scripts running in an iframe with different domain or protocol have no access to window.parent. That disables the injected iframe to communicate with the outside webpage and the injected scripts not running inside the iframe but only the outside page.

--- bye-nolike.safariextension/javascripts/inject.js
+++ bye-nolike.safariextension/javascripts/inject.js
@@ -46,4 +46,27 @@ if(needLikeToRead()) {
   displayAlert(isCrackable());
 }
 
+function injectWarningWindow() {
+  var inject = [
+    $('<p>您正在瀏覽的網頁內容包含了要求您先按下「讚」才能閱讀內容的誘導機制。這可能會導致您的身分被用作非您本意的推薦，並被網站利用為騙取信任、流量與廣告的工具。</p><p>為了避免您被網站強迫點「讚」，您可以利用下方的搜尋框在網路上搜尋是否有其他網站提供相同內容。<span class="crackable">或者您也可以選擇嘗試解除此網站上的騙「讚」機制。</span></p>'),
+    $('<form id="searchBox" action="http://www.google.com/search" method="GET" target="_top"></form>')
+    .append('<input type="search" id="q" name="q" placeholder="Google" results="0" />'),
+    $('<p class="links"><a href="#">更多資訊</a></p>'),
+    $('<form id="crackBox"><label><input type="checkbox" id="alwaysCrack" />總是嘗試破解此網站</label><a class="zbutton red" href="#">嘗試破解</a><a id="btnIgnore" class="zbutton" href="#">忽略警告</a></form>')
+  ]
+  var result = $('<div>');
+  
+  $.each(inject, function(index, item) {
+    result.append(item);
+  });
+  
+  $('<div id="nolike_container"></div>')
+    .append($('<div>')
+      .append($('<article>')
+        .append('<h1 title="警告：騙「讚」誘導網站">警告：騙「讚」誘導網站</h1>')
+        .append(result)
+      )
+    ).appendTo('body');
+}
+
 }
